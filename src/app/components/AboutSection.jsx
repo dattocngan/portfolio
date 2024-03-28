27605d0f2476fe3141dd94356239e27d19221718
Nguyen Tien Dat <nguyen.tien.dat@azoom.jp>
2024-03-28 17:13:30 +0700

    First commit

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,98 @@
+'use client'
+import React, { useTransition, useState } from 'react'
+import Image from 'next/image'
+import TabButton from './TabButton'
+
+const TAB_DATA = [
+  {
+    title: 'Skills',
+    id: 'skills',
+    content: (
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Node.js</li>
+      </ul>
+    )
+  },
+  {
+    title: 'Education',
+    id: 'education',
+    content: (
+      <ul>
+        <li>Computer Science</li>
+        <li>Information Technology</li>
+      </ul>
+    )
+  },
+  {
+    title: 'Certifications',
+    id: 'certifications',
+    content: (
+      <ul>
+        <li>React Developer</li>
+        <li>Node.js Developer</li>
+        <li>Full Stack Developer</li>
+      </ul>
+    )
+  }
+]
+
+const AboutSection = () => {
+  const [tab, setTab] = useState('skills')
+  const [isPending, startTransition] = useTransition()
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id)
+    })
+  }
+  return (
+    <section className="text-white">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image
+          src="/images/about.png"
+          alt="about image"
+          width={500}
+          height={500}
+        />
+        <div>
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil esse
+            officiis, ex in quam nam consequatur similique dignissimos tenetur
+            fugiat doloribus doloremque id dolores vero provident eos voluptate
+            temporibus deleniti.
+          </p>
+          <div className="flex flex-row mt-8">
+            <TabButton
+              selectTab={() => handleTabChange('skills')}
+              active={tab === 'skills'}
+            >
+              Skills
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange('education')}
+              active={tab === 'education'}
+            >
+              Education
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange('certifications')}
+              active={tab === 'certifications'}
+            >
+              Certifications
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default AboutSection
